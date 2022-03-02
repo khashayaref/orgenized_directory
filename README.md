bbc3eb81a60369e5c6134e9ed2d34686bc64819f
khashayar <khashayaref2020@gmail.com>
2022-03-02 18:35:37 +0330

    update OrgenizedFiles and README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # orgenized_directory
 move each file to its directories
+
+## How to Run
+First add `src` to `PYTHONPATH`
+```
+export PYTHONPATH=${PWD}
+```
+Then Run:
+```
+python src/clean.py
+```
--- src/clean.py
+++ src/clean.py
@@ -4,19 +4,13 @@ from pathlib import Path
 from src.data import DATA_DIR
 from src.utils.io import read_json
 from loguru import logger
+from typing import Union
 
 
 class OrgenizedFiles:
-    def __init__(self, path_files: str):
+    def __init__(self):
         """clean the directory by moving a file to its related directory
-
-        :param path_files: the path of the directrory
         """
-        self.path_files = Path(path_files)
-
-        if not self.path_files.exists():
-            raise FileNotFoundError(f'{self.path_files} does not exist')
-        
         extention_files = read_json(DATA_DIR / 'file_extention.json')
         extentions = {}
         for key, value in extention_files.items():
@@ -24,11 +18,17 @@ class OrgenizedFiles:
                 extentions[item] = key
         self._file_extentions = extentions
 
-    def __call__(self):
+    def __call__(self, path_files: Union[str, Path]):
         """orgenizing files by moving them to their directories
+
+        :param path_files: the file directory
         """
+        path_files = Path(path_files)
+
+        if not path_files.exists():
+            raise FileNotFoundError(f'{self.path_files} does not exist')
         extentions = []
-        for file in self.path_files.iterdir():
+        for file in path_files.iterdir():
             
             #ignore directories
             if file.is_dir():
@@ -41,14 +41,13 @@ class OrgenizedFiles:
             extentions.append(file.suffix)
             if file.suffix not in self._file_extentions:
                 continue
-            DIST_DIR = self.path_files / self._file_extentions[file.suffix]
+            DIST_DIR = path_files / self._file_extentions[file.suffix]
             DIST_DIR.mkdir(exist_ok=True)
             shutil.move(str(file), str(DIST_DIR))
             logger.info(f'Moving {file} to {DIST_DIR}')
             
 if __name__ == "__main__":
-    orgenize_file = OrgenizedFiles('/home/khashayar/Downloads')
-    orgenize_file()
+    orgenize_file = OrgenizedFiles()
+    orgenize_file('/home/khashayar/Downloads')
     print('done')
-        
         
